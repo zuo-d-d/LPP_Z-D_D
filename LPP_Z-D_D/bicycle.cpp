8f327cad09e23caf5324c52f3d54aa40d564a5ab
Zuo Daoda <zuodaoda@126.com>
2017-02-23 12:01:19 +0800

    直线初稿完成，缺赛道边界检测

--- LPP_Z-D_D/bicycle.cpp
+++ LPP_Z-D_D/bicycle.cpp
@@ -2,32 +2,69 @@
 #include "bicycle.h"
 using namespace std;
 
-float path_l(Line l_in) {
-	float    x_far = 300;                //取较远点为300
-	Point  p_m;
-	Line l_r;
-	p_m.x = x_far / 2.0;
-	p_m.y = (l_in.k * x_far + l_in.b) / 2.0;
-	l_r.k = -(p_m.x / p_m.y);
-	l_r.b = p_m.y - l_r.k * p_m.x;
-	if (fabs(l_r.b)>5000)				l_r.b=20000;	//车向稳定时设定阈值，不需调整
-	else if (fabs(l_r.b)<R_min)	l_r.b=R_min;		//超过极限半径时避免机械故障
-	return  l_r.b;
+float path_l (Line l_in) {
+	const float x_far = 300;					    //取较远点为300
+	Point p_to;
+	if (fabs(l_in.k)> Offset_k) {
+		if (l_in.k*l_in.b<0) {
+			//(1)偏角小偏距小，e-，车向稳定，返回设定阈值，不需调整
+			if (fabs(l_in.b)< Offset_m)	
+				return L;		
+			//(2)偏角小偏距大，e-
+			else 	{
+				p_to.x = x_far;
+				p_to.y = (l_in.k*x_far+l_in.b)+p_z_n(l_in.k)*Offset_m;		
+
+				/*需进行是否偏出赛道的检测，未写*/		
+				
+				return pp_to_r(p_to);						
+			}
+		}
+		else {
+			//(3)偏角小偏距小，e+
+			if (fabs(l_in.b)< Offset_m){
+				p_to.x = x_far;
+				p_to.y = (l_in.k*x_far+l_in.b)-p_z_n(l_in.k)*Offset_m;		
+
+				/*需进行是否偏出赛道的检测，未写*/		
+				
+				return pp_to_r(p_to);						
+			}
+			//(4)偏角小偏距大，e+
+			else{
+				p_to.x = x_far;							
+				p_to.y = (l_in.k*x_far+l_in.b)-p_z_n(l_in.k)*Offset_m;		
+
+				/*需进行是否偏出赛道的检测，未写*/		
+				
+				return pp_to_r(p_to);						
+			}
+		}
+	}
+	//(5)偏角大，向-k方向修正
+	else {
+		p_to.x = x_far;							
+		p_to.y = (l_in.k*x_far+l_in.b)-p_z_n(l_in.k)*Offset_m;		
+
+				/*需进行是否偏出赛道的检测，未写*/		
+				
+		return pp_to_r(p_to);				
+	}
 }
 
-float path_c(Circle c_in){
-	float r_now ;
-	const float offset = 10;
-	if (fabs(c_in.o.x)<offset) {								//车身位置良好
-		r_now = c_in.o.y;
-		if (r_now>c_in.r-100 && r_now<c_in.r)	//100为车宽加上赛道余量
-			return r_now;
-		else if (r_now<c_in.r+100)						//0.9为修正系数
-			return r_now*0.9;
-		else
-			return R_min;
-	}
-	else {															//车身位置不好
+float pp_to_r (Point p_to) {
+	Point  p_m;
+	Line l_r;
+	p_m.x = p_to.x / 2.0;
+	p_m.y = p_to.y / 2.0;
+	l_r.k = -(p_m.x / p_m.y);
+	l_r.b = p_m.y - l_r.k * p_m.x;
+	if (fabs(l_r.b)<R_min)	return R_min;		//超过极限半径时避免机械故障
+	else								return  l_r.b;
+}
 
-	}
+int p_z_n(float in){
+	if (in>0)			return 1;
+	else if (in<0)		return -1;
+	else					return 0;
 }
--- LPP_Z-D_D/bicycle.h
+++ LPP_Z-D_D/bicycle.h
@@ -1,4 +1,7 @@
 #define R_min 450
+#define L			20000
+#define Offset_k  50
+#define Offset_m 50
 // l*w=285*160
 //单位均为mm
 struct Point{
@@ -7,7 +10,7 @@ struct Point{
 };
 
 struct Circle{
-	Point o;
+	Point o; 
 	float r;
 };
 
@@ -16,4 +19,11 @@ struct Line {
 	float b;
 };
 
-float path_l(Line l_in); //取一较远点为参照，目前以车身动态坐标系为准，可能要改为赛道坐标系
+float path_l(Line l_in); 
+/*	取一较远点为参照，目前以车身动态坐标系为准，可能要改为赛道坐标系。
+	该函数输入条件为保证当前输入仅为直线。	
+*/
+
+float pp_to_r (Point p_to);
+
+int p_z_n(float in);
