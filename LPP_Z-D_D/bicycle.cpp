d64f0f9f5c6fa5cc0ab069e9b9c1c8b6c37727ed
Zuo Daoda <zuodaoda@126.com>
2017-02-24 19:40:09 +0800

    直线重构并初步测试通过

--- LPP_Z-D_D/bicycle.cpp
+++ LPP_Z-D_D/bicycle.cpp
@@ -3,59 +3,103 @@
 using namespace std;
 
 float path_l (Line l_in) {
-	const float x_far = 300;					    //取较远点为300
 	Point p_to;
 	float r_out;
-	if (fabs(l_in.k)< Offset_k) {
-		if (l_in.k*l_in.b<0) {
-			//(1)偏角小偏距小，e-，车向稳定，返回设定阈值，不需调整
-			if (fabs(l_in.b)< Offset_m)	
-				return L;		
-			//(2)偏角小偏距大，e-
-			else 	{
-				p_to.x = x_far;
-				p_to.y = (l_in.k*x_far+l_in.b)+p_z_n(l_in.k)*Offset_m;		
-				r_out = pp_to_r(p_to);
-				while (out_the_track(l_in,r_out))	r_out*=0.9;  //-10或*0.9待商讨
-				return r_out;						
+	int b_pn = -1*p_z_n(l_in.b);
+	if (fabs(l_in.b) > Offset_m) {
+		if (b_pn*l_in.k <= K_l2) {
+			p_to.x = X_far;
+			p_to.y = (l_in.k*X_far+l_in.b)+b_pn*Offset_m;		
+			r_out = pp_to_r(p_to);
+			cout<<'1'<<r_out<<endl;
+			while (out_the_track(l_in,r_out)) {
+				r_out*=0.9;  //-10或*0.9待商讨
+				if(fabs(r_out)<=R_min) {
+					r_out=p_z_n(r_out) * R_min;
+					break;
+				}
 			}
+			return r_out;						
+		}
+		else if (b_pn*l_in.k <= K_l3) {
+			p_to.x = X_far;
+			p_to.y = l_in.k*X_far+l_in.b;		
+			r_out = pp_to_r(p_to);
+			cout<<'2'<<r_out<<endl;
+			while (out_the_track(l_in,r_out)) {
+				r_out*=0.9;  //-10或*0.9待商讨
+				if(fabs(r_out)<=R_min) {
+					r_out=p_z_n(r_out) * R_min;
+					break;
+				}
+			}
+			return r_out;	
 		}
 		else {
-			//(3)偏角小偏距小，e+
-			if (fabs(l_in.b)< Offset_m){
-				p_to.x = x_far;
-				p_to.y = (l_in.k*x_far+l_in.b)-p_z_n(l_in.k)*Offset_m;		
-				r_out = pp_to_r(p_to);
-				while (out_the_track(l_in,r_out))	r_out*=0.9;  //-10或*0.9待商讨
-				return r_out;						
+			p_to.x = X_far;
+			p_to.y = (l_in.k*X_far+l_in.b)-b_pn*Offset_m;		
+			r_out = pp_to_r(p_to);
+			cout<<'3'<<r_out<<endl;
+			while (out_the_track(l_in,r_out)) {
+				r_out*=0.9;  //-10或*0.9待商讨
+				if(fabs(r_out)<=R_min) {
+					r_out=p_z_n(r_out) * R_min;
+					break;
+				}
 			}
-			//(4)偏角小偏距大，e+
-			else{
-				p_to.x = x_far;							
-				p_to.y = (l_in.k*x_far+l_in.b)-p_z_n(l_in.k)*Offset_m;		
-				r_out = pp_to_r(p_to);
-				while (out_the_track(l_in,r_out))	r_out*=0.9;  //-10或*0.9待商讨
-				return r_out;					
-			}
+			return r_out;	
 		}
 	}
-	//(5)偏角大，向-k方向修正
 	else {
-		p_to.x = x_far;							
-		p_to.y = (l_in.k*x_far+l_in.b)-p_z_n(l_in.k)*Offset_m;		
-		r_out = pp_to_r(p_to);
-		while (out_the_track(l_in,r_out))	r_out*=0.9;  //-10或*0.9待商讨
-		return r_out;			
+		if (b_pn*l_in.k < K_l1){
+			p_to.x = X_far;
+			p_to.y = (l_in.k*X_far+l_in.b)+b_pn*Offset_m;		
+			r_out = pp_to_r(p_to);
+			cout<<'4'<<r_out<<endl;
+			while (out_the_track(l_in,r_out)) {
+				r_out*=0.9;  //-10或*0.9待商讨
+				if(fabs(r_out)<=R_min) {
+					r_out=p_z_n(r_out) * R_min;
+					break;
+				}
+			}
+			return r_out;			
+		}
+		else if (b_pn*l_in.k <= K_l4){
+			cout<<'5'<<endl;
+			return L;					
+		}
+		else {
+			p_to.x = X_far;
+			p_to.y = (l_in.k*X_far+l_in.b)-b_pn*Offset_m;		
+			r_out = pp_to_r(p_to);
+			cout<<'6'<<r_out<<endl;
+			while (out_the_track(l_in,r_out)) {
+				r_out*=0.9;  //-10或*0.9待商讨
+				if(fabs(r_out)<=R_min) {
+					r_out=p_z_n(r_out) * R_min;
+					break;
+				}
+			}
+			return r_out;	
+		}
 	}
 }
 
+float path_c (Circle c_in){
+	float r_out;
+	return 0;
+}
+
 float pp_to_r (Point p_to) {
+	if (p_to.y == 0) return L;
 	Point  p_m;
 	Line l_r;
 	p_m.x = p_to.x / 2;
 	p_m.y = p_to.y / 2;
 	l_r.k = -(p_m.x / p_m.y);
 	l_r.b = p_m.y - l_r.k * p_m.x;
+	cout<<l_r.b<<endl;
 	if (fabs(l_r.b)<R_min)	return R_min;		//超过极限半径时避免机械故障
 	else								return  l_r.b;
 }
@@ -63,10 +107,9 @@ float pp_to_r (Point p_to) {
 int out_the_track(Line l_in, /*Circle c_in,*/ float r) {
 	//该版本暂不考虑圆
 	cout<<r<<endl;
-	const float x_far = 300;
 	Point p_c={0,0}, p_l={0,0};
 	float k1=l_in.k;
-	for (float x = 0; x < x_far ; x+=5)
+	for (float x = 0; x < X_far ; x+=5)
 	{
 		float y=r-pow((r*r-x*x),0.5);
 		float k=x/(r-y);
@@ -79,7 +122,9 @@ int out_the_track(Line l_in, /*Circle c_in,*/ float r) {
 	float b2=p_c.y+p_c.x/l_in.k;
 	p_l.x=(b2-l_in.b)*l_in.k/(l_in.k*l_in.k+1);
 	p_l.y=(l_in.b-b2)/(l_in.k*l_in.k+1)+b2;
+	//
 	cout<<p_c.x<<' '<<p_c.y<<endl;
+	//
 	if (pp_d(p_c,p_l) > Track_w-80 ) return 1;
 	else return 0;
 }
--- LPP_Z-D_D/bicycle.h
+++ LPP_Z-D_D/bicycle.h
@@ -1,12 +1,23 @@
 //单位均为mm
 
-#define R_min 450			//最小转弯半径
-#define L			50000		//舵机零角，是否有必要？
-#define Offset_k  0.1			//允许的方向偏移量
-#define Offset_m 20		//允许的中心线偏移量
-#define Track_w   250	//赛道半宽
+#define R_min		300 			//最小转弯半径
+#define L				50000		//舵机零角阈值，是否有必要？
+#define Offset_m	30			//允许的中心线偏移量
 
-//车 l*w=285*160
+#define K_c1		0				//<0，曲线外道内切；
+#define K_c2		0.176		//<0.176，内道内切；0~0.176，外道中线；>0.176，外道外线
+#define K_c3		0.364		//0.176~0.364，内道中线；>0.364，内道外切
+
+#define K_l1			0				//<0，直线内侧内切
+#define K_l2			0.176		//<0.176，外侧内切
+#define K_l3			0.577		//0.176~0.577，外侧中线；>0.577，外侧外切
+#define K_l4			0.466		//0~0.466，内测准直；>0.466，内测外切
+
+#define Track_w  220			//赛道半宽
+#define X_far		600			//所取的较远点x坐标
+
+//车				l*w=285*160
+//视场距离	300~600
 
 struct Point{
 	float x;
@@ -23,13 +34,14 @@ struct Line {
 	float b;
 };
 
-float path_l(Line l_in); 
-//	取一较远点为参照，目前以车身动态坐标系为准，可能要改为赛道坐标系。该函数输入条件为保证当前输入仅为直线	
+float path_l(Line l_in); //取一较远点为参照，目前以车身动态坐标系为准，可能要改为赛道坐标系。输入仅为直线	
 
-float pp_to_r (Point p_to);
+float path_c (Circle c_in);
 
-int p_z_n(float in);	 //输入一个数，判断正负
+float pp_to_r (Point p_to);				//出入两个点，求转弯半径
 
-float pp_d (Point p1, Point p2);	//求两点间距离
+int p_z_n(float in);							//输入一个数，判断正负
+
+float pp_d (Point p1, Point p2);		//求两点间距离
 
 int out_the_track(Line l_in,/* Circle c_in, */float r);	//检测当前轨迹是否会偏出赛道
